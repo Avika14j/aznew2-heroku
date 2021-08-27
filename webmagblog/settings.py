c0da4d0b12722561dd0c68009afc09d7313d23cb
AZnews <amankumarssm2365@gmail.com>
2021-08-27 11:50:14 +0530

    commit

--- webmagblog/settings.py
+++ webmagblog/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'a%jladi2qqpsj5*g*ewzs#b9yokz_7_b(1$ma!s7pfsecoz&pt'
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','aznewsc.herokuapp.com/']
 
 # Application definition
 
